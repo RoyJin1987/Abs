7205339cc7b3b784b9f6029f1f5aa02f0c6f0da3
Roy <roy.jin@jihox.com>
2014-09-07 17:54:38 +0800

    Init Abs project
    Init Abs project

---
+++ platforms/android/ant-build/Abs-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+D:\android\Abs\platforms\android\ant-build\Abs-debug-unaligned.apk : \
+D:\android\Abs\platforms\android\ant-build\Abs.ap_ \
+D:\android\Abs\platforms\android\ant-build\classes.dex \
+D:\android\Abs\platforms\android\CordovaLib\ant-build\classes.jar \
---
+++ platforms/android/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+D:\android\Abs\platforms\android\ant-build\AndroidManifest.xml : \
+D:\android\Abs\platforms\android\AndroidManifest.xml \
