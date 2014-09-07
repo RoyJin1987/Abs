db34b8cf1adcdf228aa30676f720ed204625a3c6
Roy <roy.jin@jihox.com>
2014-09-07 19:58:35 +0800

    Update ignore patterns.

--- platforms/android/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-D:\android\Abs\platforms\android\ant-build\AndroidManifest.xml : \
-D:\android\Abs\platforms\android\AndroidManifest.xml \
--- platforms/android/ant-build/classes.dex.d
+++
@@ -1,9 +0,0 @@
-D:\android\Abs\platforms\android\ant-build\classes.dex : \
-D:\android\Abs\platforms\android\ant-build\classes\com\aibangsong\abs\Abs.class \
-D:\android\Abs\platforms\android\ant-build\classes\com\aibangsong\abs\BuildConfig.class \
-D:\android\Abs\platforms\android\ant-build\classes\com\aibangsong\abs\R$attr.class \
-D:\android\Abs\platforms\android\ant-build\classes\com\aibangsong\abs\R$drawable.class \
-D:\android\Abs\platforms\android\ant-build\classes\com\aibangsong\abs\R$string.class \
-D:\android\Abs\platforms\android\ant-build\classes\com\aibangsong\abs\R$xml.class \
-D:\android\Abs\platforms\android\ant-build\classes\com\aibangsong\abs\R.class \
-D:\android\Abs\platforms\android\ant-build\dexedLibs\classes-4df1bf2667eeb9c252d824975a455176.jar \
