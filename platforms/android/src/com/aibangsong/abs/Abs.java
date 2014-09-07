43f9f86a886a10628fa2a6b27a019c86e061306c
Roy <roy.jin@jihox.com>
2014-09-07 22:20:19 +0800

    Update UI.

--- platforms/android/src/com/aibangsong/abs/Abs.java
+++ platforms/android/src/com/aibangsong/abs/Abs.java
@@ -29,8 +29,8 @@ public class Abs extends CordovaActivity
     {
         super.onCreate(savedInstanceState);
         super.init();
-        super.setIntegerProperty("splashscreen",
-        		R.drawable.splash);
+//        super.setIntegerProperty("splashscreen",
+//        		R.drawable.splash);
         // Set by <content src="index.html" /> in config.xml
         super.loadUrl(Config.getStartUrl());
         //super.loadUrl("file:///android_asset/www/index.html");
