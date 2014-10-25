ae5ec0c241fb4fe186e302cf44770c037bf7efeb
kevin <jiveszdp@126.com>
2014-10-25 22:14:50 +0800

    update UI

--- www/js/common.js
+++ www/js/common.js
@@ -42,6 +42,15 @@ var commonJS = {
         }
         return o.toString();
     },
+
+    jsonDateFormat:function(jsonDate) {//json日期格式转换为正常格式
+        var date = new Date(parseInt(jsonDate.replace("/Date(", "").replace(")/", ""), 10));
+        var month = date.getMonth() + 1 < 10 ? "0" + (date.getMonth() + 1) : date.getMonth() + 1;
+        var currentDate = date.getDate() < 10 ? "0" + date.getDate() : date.getDate();
+        return date.getFullYear() + "-" + month + "-" + currentDate;
+    },
+
+
     /**
      * LL.json
      * JSON??
