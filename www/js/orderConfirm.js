bf4d961f1e33a70d06ca2b845340031c5594f878
Roy <roy.jin@jihox.com>
2014-10-26 23:10:37 +0800

    增加调试信息

--- www/js/orderConfirm.js
+++ www/js/orderConfirm.js
@@ -64,6 +64,7 @@ var app = {
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
         app.token = $.cookie("usrToken");
+        alert("token:"+ app.token );
         //请求正在修改运单
         var orderId = app.getUrlParam("orderId");
         var key = app.getUrlParam("key");
@@ -74,9 +75,10 @@ var app = {
                 orderId:orderId,
                 Token:app.token
             };
+            alert("请求订单："+JSON.stringify(request));
             var url = app.serverUrl + JSON.stringify(request);
             commonJS.get(url,function(data){
-                // alert(JSON.stringify(data));
+                alert(JSON.stringify(data));
                 // 参数：
                 // POST|GET data={Action: "OrderConfirm",orderId: “订单编号”,key:”抢单标识”, Token:”身份令牌”} 
                 // 返回
@@ -139,7 +141,9 @@ var app = {
                     Token:app.token
                 };
                 var url = app.serverUrl + JSON.stringify(request);
+                alert("获取抢单者："+ JSON.stringify(request))
                 commonJS.get(url,function(data_){
+                    alert(JSON.stringify(data_));
                     for(var i in data_.items)
                     {
                         var pilot =  data.items[i];
