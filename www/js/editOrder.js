c9422a3a815ab6ffee74d1f114f5ff3c993f3a43
kevin <jiveszdp@126.com>
2014-12-19 10:33:32 +0800

    Update UI

--- www/js/editOrder.js
+++ www/js/editOrder.js
@@ -19,12 +19,30 @@
 
 
 var app = {
+  map:{},
+  loacalSearch:{},
+  $addressList :{},
   serverUrl:"http://112.124.122.107/Applications/web/?data=",
   token:"07b27a882cc721a9207250f1b6bd2868",
   orderId:"",
   baiduPosition:"",
   viewModel :{
-    orderInfo:{},
+    orderInfo:{
+      send_address:{
+        longitude:"",
+        latitude:"",
+        city:"上海市",
+        address:'',
+      },
+      shipping_address:{
+        longitude:"",
+        latitude:"",
+        city:"上海市",
+        address:'',
+      },
+      distance:ko.observable(-1),
+
+    },
     wenCengList :ko.observableArray(),
     modelsList: ko.observableArray(),
     selectedWenCeng:ko.observable(''),
@@ -34,10 +52,10 @@ var app = {
     send_city:"请点击选择城市", 
     shipping_city:"请点击选择城市",
     bid_item_tuijian:{ // 出价项 车型选择非卡车时才有
-          freight:60,
-          truckage:74,
-          tipping:80,
-    },  
+          freight:ko.observable(0),
+          truckage:ko.observable(0),
+          tipping:ko.observable(0),
+    }, 
   },
 
 
@@ -51,8 +69,7 @@ var app = {
 
   },
 
-    sendClick:function() {
-    
+  sendClick:function() {
         app.viewModel.orderInfo.wenCeng = app.viewModel.selectedWenCeng;
         app.viewModel.orderInfo.models = app.viewModel.selectedModels();
         
@@ -80,6 +97,20 @@ var app = {
           });
             
         }else{
+          var order = app.extractOrder();
+          if (!order) {
+            return;
+          };
+          if(app.viewModel.orderInfo.distance() >0)
+          {
+            app.viewModel.bid_item_tuijian.freight((app.viewModel.orderInfo.distance() * 0.005).toFixed(0));
+            app.viewModel.bid_item_tuijian.truckage((app.viewModel.orderInfo.distance() * 0.003).toFixed(0));
+            app.viewModel.bid_item_tuijian.tipping ((app.viewModel.orderInfo.distance() * 0.0005).toFixed(0));
+
+            app.viewModel.orderInfo.bid_item.freight(app.viewModel.bid_item_tuijian.freight());
+            app.viewModel.orderInfo.bid_item.truckage(app.viewModel.bid_item_tuijian.truckage());
+            app.viewModel.orderInfo.bid_item.tipping(app.viewModel.bid_item_tuijian.tipping());
+          }
           $.mobile.changePage("#offer");
         }
 
@@ -162,7 +193,7 @@ var app = {
           // alert(JSON.stringify(app.baiduPosition));
         }
         app.orderId = app.getUrlParam("orderId");
-         if (app.orderId) {
+        if (app.orderId) {
 
           var jsonStr = '{"Action":"getWenceng"}';
           var url = app.serverUrl +  jsonStr;
@@ -218,8 +249,17 @@ var app = {
             commonJS.get(url,function(data){
                 //alert(JSON.stringify(data));
                 app.viewModel.orderInfo = data.item;
-
+                app.viewModel.orderInfo.bid_item_tuijian={
+                  freight:ko.observable(0),
+                  truckage:ko.observable(0),
+                  tipping:ko.observable(0)
+                }
+                //绑定数据
                 app.viewModel.orderInfo.send_address.address = ko.observable(data.item.send_address.address);
+                app.viewModel.orderInfo.send_address.longitude = ko.observable(data.item.send_address.longitude);
+                app.viewModel.orderInfo.send_address.latitude = ko.observable(data.item.send_address.latitude);
+                $("#send_address").prev().find("input").val( app.viewModel.orderInfo.send_address.address());
+
                 app.viewModel.orderInfo.type = ko.observable(data.item.type);
                 app.viewModel.orderInfo.weight = ko.observable(data.item.weight);
                 app.viewModel.orderInfo.volume = ko.observable(data.item.volume);
@@ -227,7 +267,13 @@ var app = {
                 app.viewModel.orderInfo.arrival_date_ = ko.observable(data.item.arrival_date);
                 app.viewModel.orderInfo.consignee_name = ko.observable(data.item.consignee_name);
                 app.viewModel.orderInfo.consignee_phone = ko.observable(data.item.consignee_phone);
+
                 app.viewModel.orderInfo.shipping_address.address = ko.observable(data.item.shipping_address.address);
+                app.viewModel.orderInfo.shipping_address.longitude = ko.observable(data.item.shipping_address.longitude);
+                app.viewModel.orderInfo.shipping_address.latitude = ko.observable(data.item.shipping_address.latitude);
+                $("#shipping_address").prev().find("input").val( app.viewModel.orderInfo.shipping_address.address());
+                app.viewModel.orderInfo.distance = ko.observable(-1);
+
                 app.viewModel.orderInfo.delivery_floor = ko.observable(data.item.delivery_floor);
                 app.viewModel.orderInfo.additional_information = ko.observable(data.item.additional_information);
                 app.viewModel.orderInfo.selectedModels = ko.observable(data.item.models);
@@ -237,13 +283,6 @@ var app = {
                 app.viewModel.send_city = data.item.send_address.city;
                 app.viewModel.shipping_city = data.item.shipping_address.city;
 
-                // app.viewModel.orderInfo.ship_date =  ko.pureComputed(function() {
-                //    return commonJS.jsonDateFormat(data.item.ship_date_());
-                // });
-
-                // app.viewModel.orderInfo.arrival_date =  ko.pureComputed(function() {
-                //     return commonJS.jsonDateFormat(data.item.arrival_date_());
-                // });
 
                 app.viewModel.orderInfo.ship_date =  data.item.ship_date_();
                 app.viewModel.orderInfo.arrival_date =  data.item.arrival_date_();
@@ -252,6 +291,25 @@ var app = {
                 app.viewModel.orderInfo.bid_item.total = ko.pureComputed(function() {
                     return app.viewModel.orderInfo.bid_item.freight()*1+app.viewModel.orderInfo.bid_item.tipping()*1+app.viewModel.orderInfo.bid_item.truckage()*1;
                 });
+
+                if(app.viewModel.orderInfo.send_address.longitude() 
+                  && app.viewModel.orderInfo.send_address.latitude()
+                  && app.viewModel.orderInfo.shipping_address.longitude()
+                  && app.viewModel.orderInfo.shipping_address.latitude())
+                {
+                    var start = {
+                      lng:app.viewModel.orderInfo.send_address.longitude() ,
+                      lat:app.viewModel.orderInfo.send_address.latitude() 
+                    };
+                    var destination = {
+                      lng:app.viewModel.orderInfo.shipping_address.longitude(),
+                      lat:app.viewModel.orderInfo.shipping_address.latitude()
+                    }
+                    app.viewModel.orderInfo.distance(app.getDistance(start,destination).toFixed(0));
+                    app.viewModel.bid_item_tuijian.freight((app.viewModel.orderInfo.distance() * 0.005).toFixed(0));
+                    app.viewModel.bid_item_tuijian.truckage((app.viewModel.orderInfo.distance() * 0.003).toFixed(0));
+                    app.viewModel.bid_item_tuijian.tipping ((app.viewModel.orderInfo.distance() * 0.0005).toFixed(0));
+                }
 
                 ko.applyBindings(app.viewModel);
                 $('body').trigger("create");
@@ -266,14 +324,76 @@ var app = {
                   app.viewModel.selectedWenCeng(data.item.wenceng);
                   $("#wencengList").selectmenu("refresh"); 
                 }
-                // setTimeout(function(){
-                //   app.viewModel.orderInfo.bid_item.freight(100);
-                // },5000);
-                //app.viewModel.selectedModels = ko.observable("8");
-                // alert(JSON.stringify(app.viewModel));
-                // app.viewModel.consignee_name = app.viewModel.orderInfo.consignee_name();
-                // app.viewModel.consignee_phone = app.viewModel.orderInfo.consignee_phone();
             });
+
+            app.map = new BMap.Map("abs-map");        
+            app.map.centerAndZoom(app.baiduPosition, 11);
+            var options = {
+                onSearchComplete: function(results){
+                  // 判断状态是否正确
+                  if (app.loacalSearch.getStatus() == BMAP_STATUS_SUCCESS){
+                    // var s = [];
+                    var html = "";
+                    for (var i = 0; i < results.getCurrentNumPois(); i ++){
+                      // s.push(results.getPoi(i).title + ", " + results.getPoi(i).address);
+                       html += "<li><a href='#' onclick='app.bindAdrress(this)' lat="+results.getPoi(i).point.lat+" lng="+results.getPoi(i).point.lng+">" +  results.getPoi(i).title + "</a></li>";
+                    }
+                    var $ul = app.$addressList;
+                    $ul.html( html );
+                    $ul.listview( "refresh" );
+                    $ul.trigger( "updatelayout");
+                  }
+                },
+                pageCapacity:20
+
+              };
+              app.loacalSearch = new BMap.LocalSearch(app.map, options);
+
+             $( "#send_address" ).on("filterablebeforefilter", function ( e, data ) {
+              var $ul = $( this ),
+                  $input = $( data.input ),
+                  value = $input.val(),
+                  html = "";
+                  $ul.html( "" );
+                  if ( value && value.length > 2 ) {
+                      $ul.html( "<li><div class='ui-loader'><span class='ui-icon ui-icon-loading'></span></div></li>" );
+                      $ul.listview( "refresh" );
+                      app.$addressList = $ul;
+                      app.loacalSearch.search($input.val());
+                  }
+             });
+
+             $( "#shipping_address" ).on("filterablebeforefilter", function ( e, data ) {
+              var $ul = $( this ),
+                  $input = $( data.input ),
+                  value = $input.val(),
+                  html = "";
+                  $ul.html( "" );
+                  if ( value && value.length > 2 ) {
+                      $ul.html( "<li><div class='ui-loader'><span class='ui-icon ui-icon-loading'></span></div></li>" );
+                      $ul.listview( "refresh" );
+                      app.$addressList = $ul;
+                      app.loacalSearch.search($input.val());
+                  }
+             });
+             $("a.plus-expense").on("click", function ( e, data ){
+                var $self = $(this);
+                var $input = $self.parents(".form-line-wrapper").find("input");
+                if (($input.val()*1 + 10)>100000) {
+                  return;
+                }else{
+                  $input.val($input.val()*1+10);
+                }
+             });
+             $("a.minus-expense").on("click", function ( e, data ){
+                var $self = $(this);
+                var $input = $self.parents(".form-line-wrapper").find("input");
+                if (($input.val()*1 - 10)<0) {
+                  return;
+                }else{
+                  $input.val($input.val()*1-10);
+                }
+             });
         }
        
 
@@ -291,6 +411,64 @@ var app = {
         } 
 
         var order = clone(app.viewModel.orderInfo);
+
+
+        if (!app.viewModel.orderInfo.send_address.address()
+          ||app.viewModel.orderInfo.send_address.address() !== $("#send_address").prev().find("input").val() ) {
+          app.viewModel.orderInfo.send_address.address($("#send_address").prev().find("input").val());
+          app.viewModel.orderInfo.send_address.longitude("");
+          app.viewModel.orderInfo.send_address.latitude("");
+          app.viewModel.orderInfo.distance(-1);
+        };
+        if (app.viewModel.orderInfo.send_address.address()){
+          order.send_address.address = app.viewModel.orderInfo.send_address.address();
+          order.send_address.longitude = app.viewModel.orderInfo.send_address.longitude();
+          order.send_address.latitude = app.viewModel.orderInfo.send_address.latitude();
+
+        }else{
+          if (window.notificationClient){
+            window.notificationClient.showToast("请输入发货地址");  
+          }
+          return null;
+        }
+
+        if (!app.viewModel.orderInfo.shipping_address.address() 
+          ||app.viewModel.orderInfo.shipping_address.address() !== $("#shipping_address").prev().find("input").val() ) {
+          app.viewModel.orderInfo.shipping_address.address($("#shipping_address").prev().find("input").val());
+          app.viewModel.orderInfo.shipping_address.longitude("");
+          app.viewModel.orderInfo.shipping_address.latitude("");
+          app.viewModel.orderInfo.distance (-1);
+        };
+         if (app.viewModel.orderInfo.shipping_address.address()){
+          order.shipping_address.address = app.viewModel.orderInfo.shipping_address.address();
+          // order.shipping_address.city = shipCityCounty[0];
+          order.shipping_address.longitude = app.viewModel.orderInfo.shipping_address.longitude();
+          order.shipping_address.latitude = app.viewModel.orderInfo.shipping_address.latitude();
+        }else{
+          if (window.notificationClient){
+            window.notificationClient.showToast("请输入收货地址");  
+            $("#shipping_address").focus();
+          }
+          return null;
+        }
+
+
+        if (app.viewModel.orderInfo.send_address.longitude() 
+           && app.viewModel.orderInfo.send_address.latitude() 
+            && app.viewModel.orderInfo.shipping_address.longitude() 
+            && app.viewModel.orderInfo.shipping_address.latitude()) {
+          var start = {
+            lng:app.viewModel.orderInfo.send_address.longitude() ,
+            lat:app.viewModel.orderInfo.send_address.latitude() 
+          };
+          var destination = {
+            lng:app.viewModel.orderInfo.shipping_address.longitude(),
+            lat:app.viewModel.orderInfo.shipping_address.latitude()
+          }
+          app.viewModel.orderInfo.distance(app.getDistance(start,destination).toFixed(0));
+        };
+
+
 
         if (app.viewModel.orderInfo.consignee_name()){
             order.consignee_name = app.viewModel.orderInfo.consignee_name();
@@ -319,8 +497,28 @@ var app = {
 
         order.send_address.address = app.viewModel.orderInfo.send_address.address();
         order.type = app.viewModel.orderInfo.type();
-        order.weight = app.viewModel.orderInfo.weight();
-        order.volume = app.viewModel.orderInfo.volume();
+        // order.weight = app.viewModel.orderInfo.weight();
+        // order.volume = app.viewModel.orderInfo.volume();
+        if ( app.viewModel.orderInfo.weight()*1 >= 1 && app.viewModel.orderInfo.weight()*1 <= 10000) {
+          order.weight = app.viewModel.orderInfo.weight();
+        }else{
+           if (window.notificationClient){
+              window.notificationClient.showToast("货物重量应在1~10000之间");  
+              $("#weight").focus();
+            }
+            return null;
+        }
+        
+        if (app.viewModel.orderInfo.volume()*1 >=1 && app.viewModel.orderInfo.volume()*1 <= 1000) {
+           order.volume = app.viewModel.orderInfo.volume();
+        }else{
+           if (window.notificationClient){
+              window.notificationClient.showToast("货物体积应在1~1000");  
+              $("#weight").focus();
+            }
+            return null;
+        }
+     
         order.ship_date = app.viewModel.orderInfo.ship_date_();
         order.arrival_date = app.viewModel.orderInfo.arrival_date_();
         order.shipping_address.address = app.viewModel.orderInfo.shipping_address.address();
@@ -331,8 +529,6 @@ var app = {
         order.bid_item.truckage = app.viewModel.orderInfo.bid_item.truckage();
         order.bid_item.tipping = app.viewModel.orderInfo.bid_item.tipping();
         order.consignor = $.cookie("usrName");
-        order.send_address.longitude = app.baiduPosition.lng;
-        order.send_address.latitude = app.baiduPosition.lat;
 
         return order;
     },
@@ -342,5 +538,59 @@ var app = {
         var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
         var r = window.location.search.substr(1).match(reg);  //匹配目标参数
         if (r!=null) return unescape(r[2]); return null; //返回参数值
+    },
+      bindAdrress:function(btn)
+  {
+    var $li = $(btn).parent();
+    var $ul = $li.parent();
+    $ul.prev().find("input").val($(btn).text());
+    $ul.html( "" );
+    $ul.listview( "refresh" );
+    $ul.trigger( "updatelayout");
+    var point =  {
+      lng:$(btn).attr("lng"),
+      lat:$(btn).attr("lat")
     }
+    if ($ul.attr("id") === "send_address") {
+      app.viewModel.orderInfo.send_address.address ($(btn).text());
+      app.viewModel.orderInfo.send_address.longitude(point.lng);
+      app.viewModel.orderInfo.send_address.latitude (point.lat);
+    }
+    else if($ul.attr("id") === "shipping_address")
+    {
+      app.viewModel.orderInfo.shipping_address.address ($(btn).text());
+      app.viewModel.orderInfo.shipping_address.longitude(point.lng);
+      app.viewModel.orderInfo.shipping_address.latitude(point.lat);
+    }
+    if (app.viewModel.orderInfo.send_address.longitude() 
+       && app.viewModel.orderInfo.send_address.latitude() 
+        && app.viewModel.orderInfo.shipping_address.longitude() 
+        && app.viewModel.orderInfo.shipping_address.latitude()) {
+      var start = {
+        lng:app.viewModel.orderInfo.send_address.longitude() ,
+        lat:app.viewModel.orderInfo.send_address.latitude() 
+      };
+      var destination = {
+        lng:app.viewModel.orderInfo.shipping_address.longitude(),
+        lat:app.viewModel.orderInfo.shipping_address.latitude()
+      }
+      app.viewModel.orderInfo.distance(app.getDistance(start,destination).toFixed(0));
+    };
+  },
+
+  getDistance:function(p1,p2)
+  {
+      var pk = 180 / 3.1415927;
+      a1 = p1.lat / pk;
+      a2 = p1.lng / pk;
+      b1 = p2.lat / pk;
+      b2 = p2.lng / pk;
+      t1 = Math.cos(a1) * Math.cos(a2) * Math.cos(b1) * Math.cos(b2);
+      t2 = Math.cos(a1) * Math.sin(a2) * Math.cos(b1) * Math.sin(b2);
+      t3 = Math.sin(a1) * Math.sin(b1);
+      tt = Math.acos(t1 + t2 + t3);
+      return 6366000 * tt;
+  }
+
+
 };
